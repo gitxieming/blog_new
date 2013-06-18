bd24ac04fc8da618a168379d44c6dee8d90b53ad
xieming <iwhithey@163.com>
2013-06-18 15:59:06 +0800

    add msg

--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@
 var crypto = require('crypto');
 var User = require('../models/user.js');
 var Post = require('../models/post.js');
+var Comment = require('../models/comment.js');
 
 module.exports = function(app){
 	app.get('/', function(req, res){
@@ -171,6 +172,20 @@ module.exports = function(app){
 	      success: req.flash('success').toString(),
 	      error: req.flash('error').toString()
 	    });
+	  });
+	});
+	app.post('/u/:name/:day/:title', function(req,res){
+	  var date = new Date(),
+	      time = date.getFullYear() + "-" + (date.getMonth()+1) + "-" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes(),
+	      comment = {"name":req.body.name, "email":req.body.email, "website":req.body.website, "time":time, "content":req.body.content};
+	  var newComment = new Comment(req.params.name, req.params.day, req.params.title, comment);
+	  newComment.save(function(err){
+	    if(err){
+	      req.flash('error',err); 
+	      return res.redirect('/');
+	    }
+	    req.flash('success', '留言成功!');
+	    res.redirect('back');
 	  });
 	});
 };
