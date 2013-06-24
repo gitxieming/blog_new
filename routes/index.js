d5e29e6cbebbfa2e0decd09f2b564a62e8f14a06
xieming <iwhithey@163.com>
2013-06-24 17:26:54 +0800

    add tags

--- routes/index.js
+++ routes/index.js
@@ -125,7 +125,8 @@ module.exports = function(app){
 	app.post('/post', checkLogin);
 	app.post('/post', function(req, res){
 		var currentUser = req.session.user;
-		var post = new Post(currentUser.name, req.body.title, req.body.post);
+		var tags = [{"tag": req.body.tag1}, {"tag": req.body.tag2}, {"tag": req.body.tag3}];
+		var post = new Post(currentUser.name, req.body.title, tags, req.body.post);
 		post.save(function(err){
 			if(err){
 				req.flash('error', err);
@@ -149,6 +150,36 @@ module.exports = function(app){
 	    }
 	    res.render('archive',{
 	      title: '存档',
+	      posts: posts,
+	      user: req.session.user,
+	      success: req.flash('success').toString(),
+	      error: req.flash('error').toString()
+	    });
+	  });
+	});
+	app.get('/tags', function(req,res){
+	  Post.getTags(function(err, posts){
+	    if(err){
+	      req.flash('error',err); 
+	      return res.redirect('/');
+	    }
+	    res.render('tags',{
+	      title: '标签',
+	      posts: posts,
+	      user: req.session.user,
+	      success: req.flash('success').toString(),
+	      error: req.flash('error').toString()
+	    });
+	  });
+	});
+	app.get('/tags/:tag', function(req,res){
+	  Post.getTag(req.params.tag, function(err, posts){
+	    if(err){
+	      req.flash('error',err); 
+	      return res.redirect('/');
+	    }
+	    res.render('tag',{
+	      title: 'TAG:'+req.params.tag,
 	      posts: posts,
 	      user: req.session.user,
 	      success: req.flash('success').toString(),
